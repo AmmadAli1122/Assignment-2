a1ea5676ec0b69a0bedbdbdb4fbe19f1f97c5c00
AmmadAli1122 <155666572+AmmadAli1122@users.noreply.github.com>
2024-01-05 18:47:17 +0500

    Add files via upload

---
+++ Assignment#2.py
@@ -0,0 +1,18 @@
+#Task 1
+B=float(input("Enter Base Of Triangle:"))
+H=float(input("Enter Height Of Triangle:"))
+print("Area Of Triangle Is,"),print((B*H)/2,"sq.unit")
+
+#Task 2
+P1=float(input("Enter Point One:"))
+P2=float(input("Enter Point Two:"))
+print("Average Of Point One And Two Is,")
+X=((P1+P2)/2)
+print("%.2f" %X)
+        
+#Task 3
+A=float(input("Enter Radius Of Sphere:"))
+print("Volume Of The Sphere Is,")
+print((4/3)*(3.142)*(A)**3,"cub.unit")
+print("Surface Area Of The Sphere Is,")
+print(4*(3.142)*A**2,"sq.unit")
